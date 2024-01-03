200747414317877a25b4c18ec84e6fad403202ff
kwenus <kwenus82@gmail.com>
2024-01-04 02:29:28 +0300

    corr_script

--- General/Scripts/fill_music_store.sql
+++ General/Scripts/fill_music_store.sql
@@ -50,13 +50,13 @@ values ('I Decline', 118, 6), ('Queen', 230, 6), ('Fool', 235, 6), ('No Good', 2
 ('Grid', 159, 6), ('Longpig', 170, 6), ('I''m a Mother', 210, 6), ('Too Bright', 205, 6), ('All Along', 154, 6), 
 ('Peace to All Freaks', 284, 1), ('Polyaneurism', 210, 1), ('Get God''s Attention By Being An Atheist', 230, 1), ('Gypsy That Remains', 258, 1), 
 ('You''ve Had Me Everywhere', 286, 1), ('Carmillas of Love', 239, 1), ('Don''t Let Me Die in America', 215, 1), ('St. Sebastian', 245, 1), 
-('Deliberate Self-harm Ha Ha', 260, 1) ('20th Century Schizofriendic Revengoid Man', 203, 1),
-('Neal and Jack and Me', 242, 9), ('Heartbeat', 234, 9), ('Sartori in Tangier', 214, 9), ('Waiting Man', 247, 9), ('Neurotica', 288, 9)
+('Deliberate Self-harm Ha Ha', 260, 1), ('20th Century Schizofriendic Revengoid Man', 203, 1),
+('Neal and Jack and Me', 242, 9), ('Heartbeat', 234, 9), ('Sartori in Tangier', 214, 9), ('Waiting Man', 247, 9), ('Neurotica', 288, 9),
 ('Two Hands', 203, 9),('The Howler', 253, 9), ('Requiem', 408, 9),
 ('SHHH!', 194, 10), ('Valentine Shine', 181, 10), ('Predators and Scavengers', 177, 10), ('Doomsday Fatigue', 209, 10), ('Business Casual', 209, 10),
 ('Tattoo Zero', 198, 10), ('Fatback', 194, 10), ('Howlie', 248, 10), ('Eureka', 123, 10), ('Sidewinder', 234, 10), ('Recoil', 207, 10), ('Dog Eat Dog', 185, 10),
-('So Long', 192, 14), ('Seeing Eye Dog', 243, 14), ('Welcome to Algiers', 261, 14), ('LA Water', 249, 14), ('In Person', 186, 14),(Morphing', 197, 14)
-7.	"White City"	 	3:25
-8.	"And Your Bird Can Sing"	John Lennon, Paul McCartney	2:14
-9.	"Miserable"	 	3:24
-10.	"She's Lost"	 	6:30
+('So Long', 192, 14), ('Seeing Eye Dog', 243, 14), ('Welcome to Algiers', 261, 14), ('LA Water', 249, 14), ('In Person', 186, 14), ('Morphing', 197, 14),
+('White City', 205, 14), ('And Your Bird Can Sing', 134, 14), ('Miserable', 204, 14), ('She''s Lost', 390, 14),
+('Head over Heels', 218, 17), ('Turn to You', 228, 17), ('You Thought', 252, 17), ('Beneath the Blue Sky', 180, 17), ('Forget That Day', 265, 17),
+('I''m the Only One', 209, 17), ('Yes or No', 244, 17), ('Capture the Light', 195, 17), ('I''m with You', 217, 17), ('Mercenary', 220, 17)
+
